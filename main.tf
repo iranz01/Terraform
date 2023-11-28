42ec0cfd0df01922327c0c44851aa92aed54e2d2
iranz01 <iranzzzz1401@gmail.com>
2023-11-28 10:14:11 +0300

    Update main.tf to add Key_name to resource

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "ami-06dd92ecc74fdfb36"
   instance_type = "t3.micro"
+  key_name      = "Terraform-Tutorial"
 
   tags = {
+    Name       = "Linuxmaster"
     Created_By = "Terraform"
   }
 }
